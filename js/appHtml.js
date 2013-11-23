66678dbebdf445df2177daa30add576b11367f44
Brian Ephraim <brianephraim@gmail.com>
2013-11-23 16:16:46 -0500

    fixing image link

--- js/appHtml.js
+++ js/appHtml.js
@@ -9,7 +9,14 @@
 	//!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	//EXPECTS <whatev class="catSlides"></whatev> in the DOM
 	var app = function($,barndoorbuttonizer){
-		var html = '<h1 align=center>Single-image barndoor button generator</h1><p>Click this image to use it as a sample: <img src="cloud.png" class="cloud" /> . See if you can get the red, green, and blue lines to repeat!</p><DIV id="barndoorbuttonizerStatus">Or drag an image from you hard drive to the area below ...</DIV><DIV id="barndoorbuttonizerDrop">Drop files here.</DIV><DIV id="barndoorbuttonizerList"></DIV>';
+		var html = '<h1 align=center>Single-image barndoor button generator</h1><p>Click this image to use it as a sample: <img src="$@$rootPathReplaceString$@$cloud.png" class="cloud" /> . See if you can get the red, green, and blue lines to repeat!</p><DIV id="barndoorbuttonizerStatus">Or drag an image from you hard drive to the area below ...</DIV><DIV id="barndoorbuttonizerDrop">Drop files here.</DIV><DIV id="barndoorbuttonizerList"></DIV>';
+		if(document.location.host === 'localhost:4000'){
+			html = html.replace('$@$rootPathReplaceString$@$','http://localhost:8000/barndoorbuttonizer/')
+		} else if (document.location.host === 'defualt.github.io'){
+			html = html.replace('$@$rootPathReplaceString$@$','/barndoorbuttonizer/')
+		}
+
+		
 		console.log(html)
 		return html;
 	};
